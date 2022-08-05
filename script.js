f47af332cab90fcccf9b31f26ed4e4a404ed0868
erin-maxson <erin@cohatch.com>
2022-08-04 17:19:15 -0700

    updating 5 day weather cards and moving 5dayurl

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//step 1 :define all your html static selectors
+//static selectors
 var cityEl = document.querySelector("#city")
 var cityFormEl = document.querySelector("#city-form")
+
+//current weather
 var cityHeaderEl = document.querySelector("#city-header")
 var TempEl = document.querySelector("#temp")
 var windEl = document.querySelector("#wind")
@@ -8,11 +10,12 @@ var humidEl = document.querySelector("humidity")
 var uviEl = document.querySelector("#uvi")
 var cityHeaderCard1El = document.querySelector("#city-header-card-1")
 
+//5 day weather cards
 
 
 
+//open weather api key
 var api = "43307f36c133c1b4d80feb3644b2ab3e"
-//step2: make an addEventListener on Submit and create displayDashboard - it shows current weather and last five day
 
 function displayWeather(event) {
     event.preventDefault()
@@ -26,6 +29,8 @@ function displayWeather(event) {
 
         .then(function (currentData) {
             console.log(currentData)
+
+            var fiveDayUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&appid=${api}&units=imperial`
 
             fetch(fiveDayUrl)
             .then(function(response){
@@ -46,8 +51,6 @@ function displayWeather(event) {
                 console.log(humidEl.innerHTML)
                 uviEl.innerHTML = currentData.current.uvi
                 console.log(uvEl.innerHTML)
-
-                var fiveDayUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&appid=${api}&units=imperial`
                 
                 fetch(fiveDayUrl)
                 .then(function (response) {
