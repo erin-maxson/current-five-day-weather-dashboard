861b7ea6cf0a0aa6e8e627e946127f672d4510f8
erin-maxson <erin@cohatch.com>
2022-08-04 17:05:35 -0700

    reworking main card

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ var cityEl = document.querySelector("#city")
 var cityFormEl = document.querySelector("#city-form")
 var cityHeaderEl = document.querySelector("#city-header")
 var TempEl = document.querySelector("#temp")
+var windEl = document.querySelector("#wind")
+var humidEl = document.querySelector("humidity")
 var uviEl = document.querySelector("#uvi")
 var cityHeaderCard1El = document.querySelector("#city-header-card-1")
+
+
+
+
 var api = "43307f36c133c1b4d80feb3644b2ab3e"
 //step2: make an addEventListener on Submit and create displayDashboard - it shows current weather and last five day
 
@@ -32,16 +38,20 @@ function displayWeather(event) {
                 var currentDate= moment.unix(currentData.dt).format("MM/DD/YYYY")
                 var iconImage=document.createElement("img")
                 iconImage.setAttribute("src",`http://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png`)
-                cityHeaderEl.innerHTML=  currentData.name + " "+ currentDate
+                cityHeaderEl.innerHTML =  currentData.name + " "+ currentDate
                 cityHeaderEl.appendChild(iconImage)
-
-                
-                TempEl.textContent=currentData.main.temp 
+                TempEl.innerHTML =currentData.main.temp + " F"
+                console.log(TempEl.innerHTML)
+                windEl.innerHTML = currentData.wind.speed + " MPH"
+                console.log(windEl.innerHTML)
+                humidEl.textContent = currentData.main.humidity
+                console.log(humidEl.innerHTML)
+                uviEl.innerHTML = fiveDayData.current.uvi
+                console.log(uvEl.innerHTML)
                  
 
             })
         })
-
 
 }
 
